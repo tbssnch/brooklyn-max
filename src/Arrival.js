4e0926fb5dd91a023a15529305ec75d5512eda20
tb <tbussanich@gmail.com>
2018-10-07 23:23:49 -0700

    add moment to change date format from json response

--- src/Arrival.js
+++ src/Arrival.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Moment from 'react-moment';
 import './Arrival.css';
 
 class Arrival extends Component {
@@ -11,7 +12,9 @@ class Arrival extends Component {
     };
   }
 
+
   componentDidMount() {
+    const API_KEY = `${process.env.REACT_APP_TRIMET_KEY}`;
     fetch("https://developer.trimet.org/ws/V1/arrivals?locIDs=13726&appID=0BD1DE92EE497EA57B0C32698&json=true")
       .then(res => res.json())
       .then(
@@ -28,9 +31,8 @@ class Arrival extends Component {
             isLoaded: true,
             error
           });
-        }
-      )
-  }
+        })
+      }
 
   render() {
     const { error, isLoaded, arrival } = this.state;
@@ -42,14 +44,16 @@ class Arrival extends Component {
       return (
         <ul>
           {arrival.map(arrival => (
-            <li key={arrival.scheduled}>
-              {arrival.shortSign} {arrival.scheduled}
+            <li key={arrival.shortSign}>
+              <h1>{arrival.shortSign}</h1>
+              <Moment format="LT">
+                {arrival.scheduled}
+              </Moment>
             </li>
         ))}
         </ul>
       );
     }
-
   }
 }
 
