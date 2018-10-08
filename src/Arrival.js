1195e17bcf490c42399d9472c8962ce8db72b10b
tb <tbussanich@gmail.com>
2018-10-08 15:21:41 -0700

    add a little styling and eastbound arrivals

--- src/Arrival.js
+++ src/Arrival.js
@@ -15,7 +15,7 @@ class Arrival extends Component {
 
   componentDidMount() {
     const API_KEY = `${process.env.REACT_APP_TRIMET_KEY}`;
-    fetch("https://developer.trimet.org/ws/V1/arrivals?locIDs=13726&appID=0BD1DE92EE497EA57B0C32698&json=true")
+    fetch("https://developer.trimet.org/ws/V1/arrivals?locIDs=13726,13713&appID=0BD1DE92EE497EA57B0C32698&json=true")
       .then(res => res.json())
       .then(
         (resultSet) => {
@@ -42,16 +42,18 @@ class Arrival extends Component {
       return <div>Gimme a sec...</div>;
     } else {
       return (
-        <ul>
-          {arrival.map(arrival => (
-            <li key={arrival.shortSign}>
-              <h1>{arrival.shortSign}</h1>
-              <Moment format="LT">
-                {arrival.scheduled}
-              </Moment>
-            </li>
-        ))}
-        </ul>
+        <div className="going-west">
+          <ul>
+            {arrival.map(arrival => (
+              <li key={arrival.shortSign}>
+                <h1>{arrival.shortSign}</h1>
+                <Moment format="LT">
+                  {arrival.scheduled}
+                </Moment>
+              </li>
+            ))}
+          </ul>
+        </div>
       );
     }
   }
