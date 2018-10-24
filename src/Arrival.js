31631ce8db06a8c6215c7affd83c08eeff752570
tb <tbussanich@gmail.com>
2018-10-23 21:46:53 -0700

    add functionality for user to search arrivals by stop id

--- src/Arrival.js
+++ src/Arrival.js
@@ -4,37 +4,82 @@ import logo from './assets/transit-logo.png';
 import rail from './assets/rail.png';
 import './Arrival.css';
 
+import fetchArrivals from "./utils/fetchArrivals";
+
 class Arrival extends Component {
   constructor(props) {
     super(props);
     this.state = {
       error: null,
       isLoaded: false,
+      stopId: '',
       arrival: []
     };
+    this.handleOnChange = this.handleOnChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  handleOnChange(event) {
+    this.setState({
+      [event.target.name] : event.target.value
+    });
+
+  }
+
+  handleSubmit(event) {
+
+    event.preventDefault();
+    const { stopId } = this.state;
+    const API_KEY = `${process.env.REACT_APP_TRIMET_KEY}`;
+
+    fetch(`https://developer.trimet.org/ws/V1/arrivals?locIDs=${stopId}&appID=${API_KEY}&json=true`)
+    .then(res => res.json())
+    .then(
+      (res) => {
+        console.log(res);
+        this.setState({
+          isLoaded: true,
+          arrival: res.resultSet.arrival,
+        });
+        console.log(this.state);
+      },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      });  
+  }
+  
+
+
+
+
 
 
   componentDidMount() {
     const API_KEY = `${process.env.REACT_APP_TRIMET_KEY}`;
-    fetch(`https://developer.trimet.org/ws/V1/arrivals?locIDs=13726,13713&appID=${API_KEY}&json=true`)
-      .then(res => res.json())
-      .then(
-        (resultSet) => {
-          console.log(resultSet);
-          this.setState({
-            isLoaded: true,
-            arrival: resultSet.resultSet.arrival
-          });
-          console.log(this.state);
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        })
-      }
+    const { stopId } = this.state;
+
+    fetch(`https://developer.trimet.org/ws/V1/arrivals?locIDs=${stopId}&appID=${API_KEY}&json=true`)
+    .then(res => res.json())
+    .then(
+      (res) => {
+        console.log(res);
+        this.setState({
+          isLoaded: true,
+          arrival: res.resultSet.arrival,
+        });
+        console.log(this.state);
+      },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      });  
+    
+    }
 
   render() {
     const { error, isLoaded, arrival } = this.state;
@@ -47,16 +92,32 @@ class Arrival extends Component {
         <div className="arrival-container">
           <div className="arrival">
             <img className="logo" src={logo} />
-            {arrival.map(arrival => (
-              <div className="results">
-                <h3>{arrival.shortSign}</h3>
-                <h3>
-                  <Moment format="LT">
-                    {arrival.scheduled}
-                  </Moment>
-                </h3>
-              </div>
-            ))}
+            <form onSubmit={this.handleSubmit}>
+              <input
+                type="text"
+                placeholder="Stop ID"
+                onChange={this.handleOnChange}
+                value={this.state.stopId}
+                name="stopId" />
+              <input 
+                type="submit"
+                 value="Submit" />
+            </form>
+            {
+              (arrival) ?
+              arrival.map(arrival => (
+                <div key={arrival.estimated} className="results">
+                  <h3>{arrival.shortSign}</h3>
+                  <h3>
+                    <Moment format="LT">
+                      {arrival.scheduled}
+                    </Moment>
+                  </h3>
+                </div>
+              ))
+              :
+              (<h1>Enter a stop ID to see arrivals</h1>)
+            }
           </div>
           <img className="rail" src={rail} />
         </div>
