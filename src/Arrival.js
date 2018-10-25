ef288c49bb2869fc98963d791a1444dac7a87f15
tb <tbussanich@gmail.com>
2018-10-24 17:59:07 -0700

    refactor and remove componentDidMount, isLoading from state

--- src/Arrival.js
+++ src/Arrival.js
@@ -4,90 +4,40 @@ import logo from './assets/transit-logo.png';
 import rail from './assets/rail.png';
 import './Arrival.css';
 
-import fetchArrivals from "./utils/fetchArrivals";
 
 class Arrival extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      arrival: [],
       error: null,
-      isLoaded: false,
-      stopId: '',
-      arrival: []
+      stopId: ''
     };
-    this.handleOnChange = this.handleOnChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    console.log(this.state)
   }
 
-  handleOnChange(event) {
-    this.setState({
-      [event.target.name] : event.target.value
-    });
-
-  }
-
-  handleSubmit(event) {
-
-    event.preventDefault();
-    const { stopId } = this.state;
-    const API_KEY = `${process.env.REACT_APP_TRIMET_KEY}`;
-
-    fetch(`https://developer.trimet.org/ws/V1/arrivals?locIDs=${stopId}&appID=${API_KEY}&json=true`)
-    .then(res => res.json())
-    .then(
-      (res) => {
-        console.log(res);
-        this.setState({
-          isLoaded: true,
-          arrival: res.resultSet.arrival,
-        });
-        console.log(this.state);
-      },
-      (error) => {
-        this.setState({
-          isLoaded: true,
-          error
-        });
-      });  
-  }
-  
-
-
-
-
-
-
-  componentDidMount() {
-    const API_KEY = `${process.env.REACT_APP_TRIMET_KEY}`;
-    const { stopId } = this.state;
-
-    fetch(`https://developer.trimet.org/ws/V1/arrivals?locIDs=${stopId}&appID=${API_KEY}&json=true`)
-    .then(res => res.json())
-    .then(
-      (res) => {
-        console.log(res);
-        this.setState({
-          isLoaded: true,
-          arrival: res.resultSet.arrival,
-        });
-        console.log(this.state);
-      },
-      (error) => {
-        this.setState({
-          isLoaded: true,
-          error
-        });
-      });  
-    
+    fetchArrivalTimes = () => {
+      console.log("HEY")
+      const API_KEY = `${process.env.REACT_APP_TRIMET_KEY}`;
+      fetch(`https://developer.trimet.org/ws/V1/arrivals?locIDs=${this.state.stopId}&appID=${API_KEY}&json=true`)
+      .then(res => res.json())
+      .then((res) => this.setState({isLoaded: true, arrival: res.resultSet.arrival}))
+      .catch(error => this.setState({isLoaded: true, error}))
     }
 
+
+  handleChange = ({ target: {name, value} }) => this.setState ({ [name] : value })
+
+  handleSubmit = event => {
+    event.preventDefault()
+    this.fetchArrivalTimes()
+  }
+
   render() {
-    const { error, isLoaded, arrival } = this.state;
-    if (error) {
-      return <div>Error: {error.message} </div>;
-    } else if (!isLoaded) {
-      return <div>Gimme a sec...</div>;
-    } else {
+    const { error, arrival, stopId } = this.state;
+    console.log(this.state.stopId)
+    if (error) return <div>Error: {error.message}</div>
+
       return (
         <div className="arrival-container">
           <div className="arrival">
@@ -96,25 +46,22 @@ class Arrival extends Component {
               <input
                 type="text"
                 placeholder="Stop ID"
-                onChange={this.handleOnChange}
-                value={this.state.stopId}
+                onChange={this.handleChange}
+                value={stopId}
                 name="stopId" />
-              <input 
-                type="submit"
-                 value="Submit" />
+              <input type="submit" value="Submit" />
             </form>
-            {
-              (arrival) ?
-              arrival.map(arrival => (
-                <div key={arrival.estimated} className="results">
-                  <h3>{arrival.shortSign}</h3>
-                  <h3>
-                    <Moment format="LT">
-                      {arrival.scheduled}
-                    </Moment>
-                  </h3>
-                </div>
-              ))
+            {arrival.length > 0 
+            ? arrival.map(({ estimated, shortSign, scheduled }) => (
+              <div key={estimated} className="results">
+                <h3>{shortSign}</h3>
+                <h3>
+                  <Moment format="LT">
+                    {scheduled}
+                  </Moment>
+                </h3>
+              </div>
+            ))
               :
               (<h1>Enter a stop ID to see arrivals</h1>)
             }
@@ -124,6 +71,6 @@ class Arrival extends Component {
       );
     }
   }
-}
+
 
 export default Arrival;
